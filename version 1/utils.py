bca4fd491972bfa180518a4a054f94d66ff7995b
Pigeon Nation <134137685+pigeon-nation@users.noreply.github.com>
2025-02-21 13:32:59 +1100

    actually implement logging

--- version 1/utils.py
+++ version 1/utils.py
@@ -1,5 +1,14 @@
+import logging
 
+logging.basicConfig(filename='applicationlog.log', level=logging.INFO)
 
-def log(text):
-    with open("general.log", 'a') as log:
-        # need to add lines
+class Logger:
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+        self.log = self.logger.log
+        self.info = self.logger.info
+        self.warning = self.logger.warning
+        self.error = self.logger.error
+        self.critical = self.logger.critical
+        self.exception = self.logger.exception
+        self.disable = self.logger.disable
