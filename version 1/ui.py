3d83d1d38268144bec2146f6ff8320f6b616a0fa
dan13iel <danmartrendyshop@gmail.com>
2025-02-21 13:04:38 +1100

    stuff

--- version 1/ui.py
+++ version 1/ui.py
@@ -1,1 +1,21 @@
 from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", METHODS=["GET"])
+@app.route("/index.html", METHODS=["GET"])
+def homeMenu():
+    try:
+        with open("UI/index.html", 'r') as file:
+            data = file.read()
+            file.close()
+        return data, 200
+    except Exception as e:
+        
+
+
+app.run(
+    host = "0.0.0.0",
+    port = 8080,
+    debug = True
+    )
---
+++ version 1/utils.py
@@ -0,0 +1,5 @@
+
+
+def log(text):
+    with open("general.log", 'a') as log:
+        # need to add lines
